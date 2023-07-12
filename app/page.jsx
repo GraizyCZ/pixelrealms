bad6161d2200c0d5957b4e11fe9e8dbbcc4d9ac1
GraizyCZ <53063459+GraizyCZ@users.noreply.github.com>
2023-07-13 00:28:01 +0200

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Feed from "@/components/Feed";
+
+const Home = () => {
+    return (
+        <>
+            <section>
+                <h1 className="text-xl">Welcome</h1>
+            </section>
+
+            <section>
+                <Feed />
+            </section>
+        </>
+    )
+}
+
+export default Home;
